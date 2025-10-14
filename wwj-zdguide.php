c16699ba12e8facaea5a2442985c2a49d646603f
jamiesmith <hello@worldwidejamie.com>
2025-10-14 14:11:52 -0600

    Initial Commit

---
+++ wwj-zdguide.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     ZD Guide
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Make your Zendesk Help Center with WordPress
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wwj-zdguide
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wwj_Zdguide
+ */
+
+// Your code starts here.
