770c8cf5277f293f018250805b5174d9b310c075
jamiesmith <hello@worldwidejamie.com>
2025-11-07 15:59:04 -0600

    Update minimum required WordPress version to 6.2

--- wwj-zdguide.php
+++ wwj-zdguide.php
@@ -5,7 +5,7 @@
  * Plugin URI:        https://github.com/worldwidejamie/zd-guide
  * Description:       Connects to the Zendesk Guide API to build a help center in WordPress.
  * Version:           1.0
- * Requires at least: 6.1
+ * Requires at least: 6.2
  * Requires PHP:      8.0
  * Author:            Jamie Smith
  * Author URI:        https://worldwidejamie.com/
